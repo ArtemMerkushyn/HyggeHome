dbe3d4fae56a88c1fc5ac13da77e50e132417edc
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-01-17 01:24:48 +0200

    made routes, preparations for redux, connected tailwindcss

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,5 @@
+export const Main = () => {
+    return (
+        <div>Main</div>
+    );
+}
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+//import { configureStore } from '@reduxjs/toolkit';
+//import { curtSlice } from './features/cart/cartSlice.js';
+
+//export const store = configureStore({
+//    reducer: {
+//        curt: curtSlice,
+//    },
+//});

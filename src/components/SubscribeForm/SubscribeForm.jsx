30e1b94ec1c77b7a1886392df31202b55e663f8d
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2024-01-19 12:27:21 +0200

    Update SubscribeForm.jsx

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -4,12 +4,30 @@ import styles from './SubscribeForm.module.css';
 
 export default function SubscribeForm() {
     const [email, setEmail] = useState('');
+    const [isValidEmail, setValidEmail] = useState(true);
 
     function subscribe(event) {
         event.preventDefault();
-      
-        toast(`You subscribed with '${email}'`);
-        setEmail('');
+
+        if (email.trim() === '') { 
+            toast.error('The field cannot be empty.');
+            return
+        }
+
+      if (isValidEmail) {
+            toast(`You subscribed with '${email}'`);            
+            setEmail('');
+        } else {
+            toast.error('Please enter a valid email address.');
+        }      
+    }
+
+     function handleEmailChange(e) {
+        const enteredEmail = e.target.value;
+        setEmail(enteredEmail);
+
+        const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(enteredEmail);
+        setValidEmail(isValid);
     }
 
     return (
@@ -20,7 +38,7 @@ export default function SubscribeForm() {
                 className={styles.input}
                 placeholder="Your email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleEmailChange}
             />
             <button type="submit" className={styles.button}>{'>'}</button>
         </form>

eab28c068e8995db97ac4291d169e77d2d278dab
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2024-02-09 18:57:40 +0200

    Close filter after click apply
    Close filter after click apply

--- src/components/MainPageContent/ModalForm/ModalForm.jsx
+++ src/components/MainPageContent/ModalForm/ModalForm.jsx
@@ -18,7 +18,6 @@ export const ModalForm = ({ toggleModal }) => {
   const [registerUser] = useRegisterUserMutation();
 
   const handleButton = () => {
-    // Перевірка, чи всі поля заповнені
     if (
       firstName.trim() === '' ||
       lastName.trim() === '' ||
@@ -46,10 +45,10 @@ export const ModalForm = ({ toggleModal }) => {
       return false;
     }
 
-    return true; 
+    return true;
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
     if (handleButton()) {
       const newUser = {
@@ -57,111 +56,134 @@ export const ModalForm = ({ toggleModal }) => {
         lastName,
         email,
         password,
-      }
+      };
       try {
-        const result = await registerUser({email: newUser.email, password: newUser.password, first: newUser.firstName, last: newUser.lastName}); 
-        toggleModal(); 
+        await registerUser({
+          email: newUser.email,
+          password: newUser.password,
+          first: newUser.firstName,
+          last: newUser.lastName,
+        });
+        toggleModal();
         toast.success('User registered successfully');
       } catch (error) {
-
         toast.error('Failed to register user. Please try again later.');
       }
     }
   };
   return (
     <div className={css.modal_container}>
-      <button type="button" onClick={toggleModal} className={css.close_modal_button}>
+      <button
+        type="button"
+        onClick={toggleModal}
+        className={css.close_modal_button}
+      >
         <Icons icon={'cross'} />
       </button>
-      <form action="/submit" method="post" onSubmit={handleSubmit} className={css.modal_form}>
-      <h2 className={css.modal_register_text}>Register Individual Account!</h2>
+      <form
+        action="/submit"
+        method="post"
+        onSubmit={handleSubmit}
+        className={css.modal_form}
+      >
+        <h2 className={css.modal_register_text}>
+          Register Individual Account!
+        </h2>
 
-<MyInput
-    type="text"
-    id="first-name"
-    name="first-name"
-    placeholder="Your first name"
-    value={firstName}
-    onChange={(e) => setFirstName(e.target.value)}
-    labelFor='Your first name*'
-/>
+        <MyInput
+          type="text"
+          id="first-name"
+          name="first-name"
+          placeholder="Your first name"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+          labelFor="Your first name*"
+        />
 
-<MyInput
-  type="text"
-  id="last-name"
-  name="last-name"
-  placeholder="Your last name"
-  value={lastName}
-  onChange={(e) => setLastName(e.target.value)}
-  labelFor='Your last name*'
-/>
+        <MyInput
+          type="text"
+          id="last-name"
+          name="last-name"
+          placeholder="Your last name"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+          labelFor="Your last name*"
+        />
 
-<MyInput
-  type="text"
-  id="email"
-  name="email"
-  placeholder="Your email"
-  value={email}
-  onChange={(e) => setEmail(e.target.value)}
-  labelFor='Your Email*'
-/>
+        <MyInput
+          type="text"
+          id="email"
+          name="email"
+          placeholder="Your email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          labelFor="Your Email*"
+        />
 
-<MyInput
-  type="password"
-  id="password"
-  name="password"
-  placeholder="Password"
-  value={password}
-  onChange={(e) => setPassword(e.target.value)}
-  labelFor='Create password*'
-/>
+        <MyInput
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          labelFor="Create password*"
+        />
 
-<MyInput
-  type="password"
-  id="confirm-password"
-  name="confirm-password"
-  placeholder="Password"
-  value={confirmPassword}
-  onChange={(e) => setConfirmPassword(e.target.value)}
-  labelFor='Confirm the password*'
-/>
+        <MyInput
+          type="password"
+          id="confirm-password"
+          name="confirm-password"
+          placeholder="Password"
+          value={confirmPassword}
+          onChange={e => setConfirmPassword(e.target.value)}
+          labelFor="Confirm the password*"
+        />
 
-<div className={css.modal_checkbox_container}>
-  <button
-    type="button"
-    className={`${css.modal_checkbox} ${firstCheckbox ? css.checked : ''}`}
-    onClick={() => {
-      setFirstCheckbox((prev) => !prev);
-    }}
-  >
-    <div className={css.svg_div}>
-      <Icons icon={'check'} />
-    </div>
-  </button>
-  <label htmlFor="agreement" className={css.agreement_text}>
-    I accept the Purchase Rules, User Agreements, and Privacy Policy conditions
-  </label>
-</div>
+        <div className={css.modal_checkbox_container}>
+          <button
+            type="button"
+            className={`${css.modal_checkbox} ${
+              firstCheckbox ? css.checked : ''
+            }`}
+            onClick={() => {
+              setFirstCheckbox(prev => !prev);
+            }}
+          >
+            <div className={css.svg_div}>
+              <Icons icon={'check'} />
+            </div>
+          </button>
+          <label htmlFor="agreement" className={css.agreement_text}>
+            I accept the Purchase Rules, User Agreements, and Privacy Policy
+            conditions
+          </label>
+        </div>
 
-<div className={css.modal_checkbox_container}>
-  <button
-    type="button"
-    className={`${css.modal_checkbox} ${secondCheckbox ? css.checked : ''}`}
-    onClick={() => {
-      setSecondCheckbox((prev) => !prev);
-    }}
-  >
-    <div className={css.svg_div}>
-      <Icons icon={'check'} />
-    </div>
-  </button>
-  <label htmlFor="agreement" className={css.agreement_text}>
-    I agree to receive emails about the new offers from HyggeHome
-  </label>
-</div>
+        <div className={css.modal_checkbox_container}>
+          <button
+            type="button"
+            className={`${css.modal_checkbox} ${
+              secondCheckbox ? css.checked : ''
+            }`}
+            onClick={() => {
+              setSecondCheckbox(prev => !prev);
+            }}
+          >
+            <div className={css.svg_div}>
+              <Icons icon={'check'} />
+            </div>
+          </button>
+          <label htmlFor="agreement" className={css.agreement_text}>
+            I agree to receive emails about the new offers from HyggeHome
+          </label>
+        </div>
 
-<Button text="Create account" style={{ width: "426px",  heigth : "48px"}} type="submit" />
-
+        <Button
+          text="Create account"
+          style={{ width: '426px', heigth: '48px' }}
+          type="submit"
+        />
       </form>
     </div>
   );

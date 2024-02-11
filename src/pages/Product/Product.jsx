80402507db06336f6a4f9b413c329d212a0e467e
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-02-11 18:05:03 +0200

    wrote the navigation in the product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
 import { ProductTop } from '../../components/ProductTop/ProductTop';
+import Icons from '../../components/Icons/Icons';
+import styles from './Product.module.css';
 
 export const Product = () => {
   return (
     <div>
+      <div className={styles.navigation}>
+        <Link to={'/'}>Home</Link>
+        <Icons icon='next'/>
+        <span>Product name</span>
+      </div>
       <ProductTop />
     </div>
   );

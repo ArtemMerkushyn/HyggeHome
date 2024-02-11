fa007df6f40222269f2faee775a9de2024e1b7d1
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-02-11 20:48:14 +0200

    showing some data on the ProductTop component

--- src/components/ProductTop/SliderNoArrow/SliderNoArrow.jsx
+++ src/components/ProductTop/SliderNoArrow/SliderNoArrow.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 import styles from './SliderNoArrow.module.css';
 
-const productImgs = [
-    'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704924093841-9OV3ODVYUGCSWO2KNBZJ/Hygge_23_Jar+MockUp-Milk+Honey.png',
-    'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704923116876-P2JPUSIRZT48HFTPPZSC/Hygge_23_Jar+MockUp-Eucalyptus+Lavender.png',
-    'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704923862189-7IDAGHALJ8TK8RLM02B5/Hygge_23_Jar+MockUp-Sunday+Morning.png',
-    'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704922573056-ER7UDG642B5TVQJN28HP/Hygge_23_Jar+MockUp-Vanilla+Cashmere.png',
-    'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704924297087-7WHUEI23YDPC31KJ9FJV/Hygge_23_Jar+MockUp-Berries+N+Cream.png',
-]
-
-export const SliderNoArrow = () => {
+export const SliderNoArrow = ({data}) => {
     const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
     const handleDotClick = (index) => {
         setSelectedImageIndex(index);
     }
+
+    const productImgs = [
+        data.image,
+        'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704923116876-P2JPUSIRZT48HFTPPZSC/Hygge_23_Jar+MockUp-Eucalyptus+Lavender.png',
+        'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704923862189-7IDAGHALJ8TK8RLM02B5/Hygge_23_Jar+MockUp-Sunday+Morning.png',
+        'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704922573056-ER7UDG642B5TVQJN28HP/Hygge_23_Jar+MockUp-Vanilla+Cashmere.png',
+        'https://images.squarespace-cdn.com/content/v1/60411a8c24cd730f88e69740/1704924297087-7WHUEI23YDPC31KJ9FJV/Hygge_23_Jar+MockUp-Berries+N+Cream.png',
+    ];
 
     return (
         <div className={styles.slider}>

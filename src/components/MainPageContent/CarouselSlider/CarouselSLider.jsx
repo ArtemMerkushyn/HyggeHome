e2a6fbd61c728647e9ef28d25d863f5e46b980ea
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-02-13 15:32:07 +0100

    .

--- src/components/MainPageContent/CarouselSlider/CarouselSLider.jsx
+++ src/components/MainPageContent/CarouselSlider/CarouselSLider.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './CarouselSlider.module.css'
 
-export const CarouselSlider = ({scrollBarPosition, widthScrollNav}) => {
+const CarouselSlider = ({scrollBarPosition, widthScrollNav}) => {
     return (
         <div className={styles.scroll}>
         <div className={styles.scrollnav} style={{ transform: `translateX(${scrollBarPosition}px)`, width: `${widthScrollNav}px` }}></div>
       </div>
     );
 };
+export default CarouselSlider

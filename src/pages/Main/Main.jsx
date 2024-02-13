6cd5080f19cd5b83f1a497a0da456d64a07847fd
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-02-13 15:33:21 +0100

    Fixed modal window

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -24,7 +24,13 @@ export const Main = () => {
   return (
     <div>
       <Hero />
-      <NewCollection catalog={catalog} error={error} isLoading={isLoading} upperText='New collection' lowerText='New Hygge goods for comfort'/>
+      <NewCollection 
+      catalog={catalog} 
+      error={error} 
+      isLoading={isLoading} 
+      upperText='New collection' 
+      lowerText='New Hygge goods for comfort'
+      sliderNeed={true}/>
       <SpecialOffer />
     </div>
   );

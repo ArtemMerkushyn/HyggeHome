02f0197ad7d465d21a68d0abd4b31f77197237d5
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2024-02-13 14:20:53 +0200

    Create product navigation

---
+++ src/components/ProductNavigation/Questions/Questions.jsx
@@ -0,0 +1,7 @@
+export default function Questions() {
+  return (
+    <div>
+      <h1>Welcome to Questions</h1>
+    </div>
+  );
+}
---
+++ src/components/ProductNavigation/Reviews/Reviews.jsx
@@ -0,0 +1,7 @@
+export default function Reviews() {
+  return (
+    <div>
+      <h1>Welcome to Reviews</h1>
+    </div>
+  );
+}

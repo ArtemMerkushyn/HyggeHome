d37a50b8f490220b2239f42ea776edaf26314a09
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-02-17 01:51:34 +0200

    deleted the console log

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -44,7 +44,6 @@ export default function Filters({
   const [filteredData, setFilteredData] = useState([]);
   const prevDataRef = useRef([]);
   const sortValue = useSelector(state => state.filter.sortValue);
-  console.log(sortValue);
 
   const { data, isLoading } = useGetFilterPriceQuery(
     { min: minV, max: maxV },

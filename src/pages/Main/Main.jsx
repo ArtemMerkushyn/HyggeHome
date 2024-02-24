aa583f3c7b8d5f9f6e79922faa5c74204d67bd66
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-02-24 16:28:14 +0200

    Your wish list
    Your wish list

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,38 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useGetCandlesQuery } from '../../redux/services';
+
 import { Hero } from '../../components/MainPageContent/Hero/Hero';
 import { SpecialOffer } from '../../components/MainPageContent/SpecialOffer/SpecialOffer';
 import { NewCollection } from '../../components/MainPageContent/secondMainContent/NewCollection';
 import { setIsActive } from '../../redux/slices/searchSlice';
 
 export const Main = () => {
-  const [catalog, setCatalog] = useState([]);
-  const { data, error, isLoading } = useGetCandlesQuery();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setIsActive(false));
   }, [dispatch]);
 
-  useEffect(() => {
-    if (data) {
-      setCatalog(data);
-    }
-  }, [data]);
-
   return (
     <div>
       <Hero />
-      <NewCollection
-        sliderNeeded={true}
-        catalog={catalog}
-        error={error}
-        i
-        sLoading={isLoading}
-        upperText="New collection"
-        lowerText="New Hygge goods for comfort"
-      />
+      <div style={{ marginTop: '120px' }}>
+        <NewCollection
+          sliderNeeded={true}
+          upperText="New collection"
+          lowerText="New Hygge goods for comfort"
+        />
+      </div>
       <SpecialOffer />
     </div>
   );
--- src/pages/Wish/Wish.jsx
+++ src/pages/Wish/Wish.jsx
@@ -1,36 +1,32 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import styles from './Wish.module.css';
-import { useEffect, useState } from 'react';
-import { useGetCandlesQuery } from '../../redux/services';
+
 import { NewCollection } from '../../components/MainPageContent/secondMainContent/NewCollection';
+import CardList from '../../components/CardList/CardList';
+import { selectFavorites } from '../../redux/selectors';
+import styles from './Wish.module.css';
 
 export const Wish = () => {
-    const { data, isLoading } = useGetCandlesQuery();
-    const [loadedData, setLoadedData] = useState([]);
+  const favoritList = useSelector(selectFavorites);
 
-    useEffect(() => {
-        if (!isLoading && data) {
-            setLoadedData(data);
-        }
-    }, [data, isLoading]);
-
-    return (
-        <div className={styles.wish}>
-            <div className={styles.nav}>
-                <div className={styles.nav__item}>
-                    <span>Your wish list</span>
-                </div>
-                <div className={styles.nav__item}>
-                    <Link to={'/curt'}>Your cart</Link>
-                </div>
-            </div>
-            <div className={styles.line}></div>
-            <NewCollection
-                sliderNeeded={false}
-                catalog={loadedData} 
-                upperText='products for you' 
-                lowerText='You might also like'
-            />
+  return (
+    <div className={styles.wish}>
+      <div className={styles.nav}>
+        <div className={styles.nav__item}>
+          <span>Your wish list</span>
         </div>
-    );
-}
+        <div className={styles.nav__item}>
+          <Link to={'/curt'}>Your cart</Link>
+        </div>
+      </div>
+
+      <div className={styles.line}></div>
+      <CardList data={favoritList} />
+      <NewCollection
+        sliderNeeded={false}
+        upperText="products for you"
+        lowerText="You might also like"
+      />
+    </div>
+  );
+};

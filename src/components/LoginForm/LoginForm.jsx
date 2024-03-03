1b49b79b3b44bd25a8fd2bf2b4214c37462da2aa
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-03-03 01:28:31 +0100

    commit

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,108 +1,113 @@
 import React, { useState } from 'react';
-import css from './LoginForm.module.css'
+import css from './LoginForm.module.css';
 import MyInput from '../UI/MyInput';
 import Icons from '../Icons/Icons';
 import Button from '../UI/Button/Button';
+import { useFormik } from 'formik';
+import { formSchema } from '../../schemas/formSchema';
+
+const LoginForm = ({ closeModal, handleRegisterClick }) => {
+
+        const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(values);
+        closeModal()
+    }
+
+      const {values, errors, touched, handleBlur, handleChange, handleSubmit} = useFormik({
+    initialValues: {
+              email: "",
+            password: "",
+    }, 
+    validationSchema: formSchema,
+    onSubmit
+  })
+
+    const [passwordVisibility, setPasswordVisibility] = useState(false);
+    const [checkbox, setCheckbox] = useState(false);
 
 
-const LoginForm = ({closeModal, handleRegisterClick}) => {
-
-    const [formValues, setFormValues] = useState({
-        email: '',
-        password: '',
-    });
-    const [passwordVisability, setPasswordVisability] = useState(false)
-    const [checkbox, setCheckbox] = useState(false)
-    
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormValues({ ...formValues, [name]: value });
-    };
-    
-
-    return (
-        <ul className={css.container}>
-            <li>
-            <h2 className={css.logIn_text}>Log in</h2>
-            <form>
-            <MyInput 
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Your email" 
-                labelFor="Your email or phone number*"
-                value={formValues.email}
-                onChange={handleChange}
-            />
-
-            <div className={css.hide_password}>  
-            <MyInput 
-                type={passwordVisability ? "text" : "password"}
-                id="password"
-                name="password"
-                placeholder="Password"
-                labelFor="Password*"
-                value={formValues.password}
-                onChange={handleChange}
-                />
-                <button
-                className={css.hide_password_button}
-                type='button'
-                onClick={() => setPasswordVisability(!passwordVisability)}
-                 >
-                {passwordVisability ? <Icons icon={'eye'} /> : <Icons icon={'closed_eye'}/> }
-                    </button>
-                    <div className={css.remember_and_remind}>
-                        <div className={css.modal_checkbox_container}>
+    return (       
+        <>
+        <div className={css.container}>
+            
+                <form onSubmit={handleSubmit}>
+                    <h2 className={css.logIn_text}>Log in</h2>
+                    <MyInput
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder="Your email"
+                        labelFor="Your email or phone number*"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        errorField={errors.email}
+                        touched={touched.email}
+                    />
+                    <div className={css.hide_password}>
+                        <MyInput
+                            type={passwordVisibility ? "text" : "password"}
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            labelFor="Password*"
+                            value={values.password}
+                            onChange={handleChange}
+                        />
                         <button
-                        type="button"
-                        className={`${css.modal_checkbox} ${
-                        checkbox ? css.checked : ''
-                        }`}
-                        onClick={() => {
-                        setCheckbox(prev => !prev);
-                        }}
+                            className={css.hide_password_button}
+                            type='button'
+                            onClick={() => setPasswordVisibility(!passwordVisibility)}
                         >
-                        <div className={css.svg_div}>
-                        <Icons icon={'check'} />
-                        </div>
+                            {passwordVisibility ? <Icons icon={'eye'} /> : <Icons icon={'closed_eye'} />}
                         </button>
-                        <label htmlFor="remember" className={css.remember_me}>
-                        Remember me
-                        </label>
+                        <div className={css.remember_and_remind}>
+                            <div className={css.modal_checkbox_container}>
+                                <button
+                                    type="button"
+                                    className={`${css.modal_checkbox} ${checkbox ? css.checked : ''}`}
+                                    onClick={() => {
+                                        setCheckbox(prev => !prev);
+                                    }}
+                                >
+                                    <div className={css.svg_div}>
+                                        <Icons icon={'check'} />
+                                    </div>
+                                </button>
+                                <label htmlFor="remember" className={css.remember_me}>
+                                    Remember me
+                                </label>
+                            </div>
+                            <button className={css.remind_password} onClick={() => { alert('ФУНКЦІОНАЛ НЕ ЗАВЕЗЛИ!!!!') }}>Remind password</button>
                         </div>
-                        <button className={css.remind_password} onClick={() =>{ alert('ФУНКЦІОНАЛ НЕ ЗАВЕЗЛИ!!!!')}}>Remind password</button>
                     </div>
-                </div>
-                <div className={css.buttonDiv}>
-                <Button
-                    text='Log in'
-                    type='submit'
-                    style={{ width: '250px', marginTop: '23px' }}
-                    funcClick={closeModal}
-                    
-                />
-                <Button
-                    type='submit'
-                    text='Sign in'
-                    buttonType='outlined'
-                    funcClick={handleRegisterClick}
-                    style={{textDecoration: 'underline'}}
-                />
+                    <div className={css.buttonDiv}>
+                        <Button
+                            text='Log in'
+                            type={"submit"}
+                            style={{ width: '250px', marginTop: '23px' }}
+                            funcClick={onSubmit}
+                        />
+                        <Button
+                            type='button'
+                            text='Sign in'
+                            buttonType='outlined'
+                            funcClick={handleRegisterClick}
+                            style={{ textDecoration: 'underline' }}
+                        />
                     </div>
                 </form>
-            </li>
-            <li className={css.div_li}>
+            <div className={css.div_li}>
                 <div className={css.hr}></div>
-            </li>
-            <li className={css.logIn_with_container}>
+            </div>
+            <div className={css.logIn_with_container}>
                 <h2 className={css.logIn_with}>Log in with</h2>
-                <button className={css.logIn_with_button}><Icons icon='google'/>Google</button>
-                <button className={css.logIn_with_button}><Icons icon='facebook'/>Facebook</button>
-            </li>
-
-
-        </ul>
+                <button className={css.logIn_with_button} type='button'><Icons icon='google' />Google</button>
+                <button className={css.logIn_with_button} type='button'><Icons icon='facebook' />Facebook</button>
+            </div>
+            </div>
+            </>
     );
 };
 

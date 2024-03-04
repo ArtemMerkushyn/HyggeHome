6c753b1b2b3ec947bd7a491715d6fce035735c35
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-03-04 17:55:19 +0200

    wrote the Delivery component

--- src/pages/CurtPage/Delivery/Delivery.jsx
+++ src/pages/CurtPage/Delivery/Delivery.jsx
@@ -1,7 +1,78 @@
+import { useState } from 'react';
+import styles from './Delivery.module.css';
 
+const inputFields = [
+    { label: 'Your first name*', placeholder: 'Your first name', id: 'firstName', name: 'firstName' },
+    { label: 'Your last name*', placeholder: 'Your last name', id: 'lastName', name: 'lastName' },
+    { label: 'Your address*', placeholder: 'Your address', id: 'address', name: 'address' },
+    { label: 'Your city*', placeholder: 'Your city', id: 'city', name: 'city' },
+    { label: 'Postal code*', placeholder: 'Postal code', id: 'postalCode', name: 'postalCode' },
+    { label: 'Phone number*', placeholder: 'Phone number', id: 'phoneNumber', name: 'phoneNumber', type: 'tel' }
+];
 
 export const Delivery = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        address: '',
+        city: '',
+        postalCode: '',
+        phoneNumber: '',
+        option: '5',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({ ...prevState, [name]: value }));
+    }
+
+    const handleOptionChange = (e) => {
+        const { value } = e.target;
+        setFormData(prevState => ({ ...prevState, option: value }));
+    }
+
     return (
-        <div>Delivery</div>
+        <form onSubmit={(e) => e.preventDefault()}>
+            {inputFields.map(field => (
+                <div className={styles.field} key={field.id}>
+                    <label htmlFor={field.id}>{field.label}</label>
+                    <input 
+                        type={'text'}
+                        placeholder={field.placeholder}
+                        id={field.id}
+                        name={field.name}
+                        value={formData[field.name]}
+                        onChange={handleChange}
+                        required 
+                    />
+                </div> 
+            ))}
+            <h3 className={styles.title}>Delivery method</h3>
+            <div className={styles.wrapper}>
+                {['5', '10', '9'].map(metod => (
+                    <label className={styles.metod} key={metod}>
+                        <input 
+                            type="radio" 
+                            name="option" 
+                            value={metod} 
+                            checked={formData.option === metod} 
+                            onChange={handleOptionChange} 
+                            style={{ display: 'none' }}
+                            required 
+                        />
+                        <div className={styles.radio} htmlFor={metod}>
+                            <span
+                                style={{ backgroundColor: formData.option === metod ? '#FCB654' : 'transparent' }}
+                            ></span>
+                        </div>
+                        <div className={styles.metod__img}></div>
+                        <div className={styles.metod__info}>
+                            <h5>Delivery method</h5>
+                            <span>${metod}</span>
+                        </div>
+                    </label>
+                ))}
+            </div>
+        </form>
     );
 }

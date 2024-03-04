32846ab4ebedd92368632fe679190b57ed5bb685
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-03-04 01:59:20 +0100

    Added changes to login feature

--- src/components/FacebookLogin/CustomFacebookLoginButton.jsx
+++ src/components/FacebookLogin/CustomFacebookLoginButton.jsx
@@ -1,39 +1,38 @@
-import React, { useCallback, useState } from 'react';
-import { LoginSocialFacebook } from 'reactjs-social-login';
-import css from '../LoginForm/LoginForm.module.css'
+import React, { useState } from 'react';
+import {LoginSocialFacebook} from 'reactjs-social-login';
+import css from '../LoginForm/LoginForm.module.css';
 import Icons from '../Icons/Icons';
 import { toast } from 'react-toastify';
 
 const REDIRECT_URI = window.location.href;
 
 const CustomFacebookLoginButton = ({ modalAction }) => {
-    
-    const [provider, setProvider] = useState('')
-  const [profile, setProfile] = useState(null)
-  const onLoginStart = useCallback(() => {
-    alert('login start')
-  }, [])
-    
-    return (
+  const [provider, setProvider] = useState('');
+  const [profile, setProfile] = useState(null);
+  
+  return (
     <LoginSocialFacebook
-            isOnlyGetToken={false}
-            appId={process.env.REACT_APP_FB_APP_ID || '1574643319992056'}
-            fieldsProfile={
+          appId={process.env.REACT_APP_FB_APP_ID || '1574643319992056'}
+          fieldsProfile={
             'id,first_name,last_name,middle_name,name,name_format,picture,short_name,email,gender'
           }
-            onLoginStart={onLoginStart}
-            onResolve={({ provider, data }) => {
-              setProvider(provider)
-                setProfile(data)
-                console.log(profile)
-            }}
-            onReject={(err) => {
-              console.log(err)
-            }}
-          >
-            <span><button className={css.logIn_with_button} type='button'><Icons icon='facebook' />Facebook</button></span>
-        </LoginSocialFacebook>
-    );
+          redirect_uri={REDIRECT_URI}
+          onResolve={({ provider, data }) => {
+            setProvider(provider);
+            setProfile(data);
+          }}
+          onReject={err => {
+            console.log(err);
+          }}
+        >
+      <span>
+        <button className={css.logIn_with_button} type='button'>
+          <Icons icon='facebook' />
+          Facebook
+        </button>
+      </span>
+    </LoginSocialFacebook>
+  );
 };
 
 export default CustomFacebookLoginButton;

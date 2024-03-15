924f870202e7dd3bfe4f3ef2428aaf773ef508b4
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-03-15 19:10:39 +0200

    fixed problems on the cart page

--- src/pages/CurtPage/CartPage.jsx
+++ src/pages/CurtPage/CartPage.jsx
@@ -1,11 +1,8 @@
-import styles from './CartPage.module.css';
 import { Cart } from './Cart/Cart';
 import { Delivery } from './Delivery/Delivery';
 import { Payment } from './Payment/Payment';
-import Button from '../../components/UI/Button/Button';
 import { TabSwitcher } from './TabSwitcher/TabSwitcher';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { NewCollection } from '../../components/MainPageContent/secondMainContent/NewCollection';
 
 const tabs = [
@@ -17,33 +14,9 @@ const tabs = [
 export const CartPage = () => {
   const [selectedId, setSelectedId] = useState(tabs[0].id);
 
-  const navigate = useNavigate();
-
-  const handlerNext = () => {
-    const currentIndex = tabs.findIndex(tab => tab.id === selectedId);
-    if(currentIndex === 2) {
-      navigate('/');
-    }
-    // Переключаемся на следующий элемент, оборачиваясь к началу, если мы достигли конца массива
-    const nextIndex = (currentIndex + 1) % tabs.length;
-    setSelectedId(tabs[nextIndex].id);
-  }
-
-  const handlerPrev = () => {
-    const currentIndex = tabs.findIndex(tab => tab.id === selectedId);
-    const nextIndex = (currentIndex - 1) % tabs.length;
-    setSelectedId(tabs[nextIndex].id);
-  }
-
   return (
     <div style={{ paddingBottom: selectedId === tabs[0].id ? '120px': 0 }}>
       <TabSwitcher tabs={tabs} selectedId={selectedId} setSelectedId={setSelectedId}/>
-      <div className={styles.btn} style={{ margin: selectedId === tabs[0].id ? '-196px 0 120px' : '60px 0 120px' }}>
-        {selectedId !== tabs[0].id ? (
-          <button className={styles.btnPrev} onClick={handlerPrev}>Previous step</button>
-        ) : null}
-        <Button text={'Next step'} funcClick={handlerNext} />
-      </div>
       {selectedId === tabs[0].id ? (
         <NewCollection
           sliderNeeded={false}

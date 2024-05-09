4851f045a94f9c6a591ac5ac82cbd4df13062589
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-05-10 00:53:13 +0200

    Added responsive styles for tabSwitcher + prettier new collection

--- src/pages/CurtPage/TabSwitcher/TabSwitcher.jsx
+++ src/pages/CurtPage/TabSwitcher/TabSwitcher.jsx
@@ -7,121 +7,135 @@ import { toast } from 'react-toastify';
 import validator from 'validator';
 
 export const TabSwitcher = ({ tabs, selectedId, setSelectedId }) => {
-    const curtItems = useSelector(selectCurtProducts);
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        address: '',
-        city: '',
-        postalCode: '',
-        phoneNumber: '',
-        email: '',
-        optionDeliveryMethod: '5',
-    });
+  const curtItems = useSelector(selectCurtProducts);
+  const scrollContainer = document.querySelector(`.${styles.scrollContainer}`);
+  const { scrollLeft } = scrollContainer;
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    address: '',
+    city: '',
+    postalCode: '',
+    phoneNumber: '',
+    email: '',
+    optionDeliveryMethod: '5',
+  });
 
-    const PaymentMethod = [
-        { id: 1, method: 'Payment by card', commission: 'No commission' },
-        { id: 2, method: 'Google pay', commission: 'No commission' },
-    ];
+  const PaymentMethod = [
+    { id: 1, method: 'Payment by card', commission: 'No commission' },
+    { id: 2, method: 'Google pay', commission: 'No commission' },
+  ];
 
-    const [ paymentMethod, setPaymentMethod ] = useState(PaymentMethod[0]);
-    const [ rules, setRules ] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState(PaymentMethod[0]);
+  const [rules, setRules] = useState(false);
 
-    const handleClick = (tabId) => {
-        if (curtItems.length === 0) {
-            toast('your cart is empty');
-            return;
-        }
-    
-        if (tabId === tabs[0].id) {
-            setSelectedId(tabId);
-            return;
-        }
-    
-        if (selectedId === tabs[0].id) { 
-            setSelectedId(tabs[1].id);
-            return;
-        }
-    
-        if (selectedId === tabs[1].id) {
-            if (
-                formData.firstName === '' ||
-                formData.lastName === '' ||
-                formData.address === '' ||
-                formData.city === '' ||
-                formData.postalCode === '' ||
-                formData.optionDeliveryMethod === ''
-            ) {
-                toast('Please fill in all fields');
-                return;
-            }
-    
-            if (formData.phoneNumber === '') {
-                if (!/^\d{9}$/.test(formData['phoneNumber'])) {
-                    toast('Please enter a valid phone number with 9 digits');
-                    return;
-                }
-            }
-    
-            if (formData.email === '') {
-                toast('Please fill in email field');
-                return;
-            }
-    
-            if (!validator.isEmail(formData.email)) {
-                toast('Please enter a valid email address');
-                return;
-            }
-        }
-        setSelectedId(tabId);
+  const handleClick = tabId => {
+    if (curtItems.length === 0) {
+      toast('your cart is empty');
+      return;
     }
-    
-    
-    const lineWidth = (tabs.findIndex(t => t.id === selectedId) + 1) * (100 / tabs.length);
 
-    return (
-        <div>
-            <div className={styles.wrapper}>
-                {tabs.map((tab, index) => (
-                    <button
-                        className={styles.title}
-                        key={tab.id}
-                        onClick={selectedId !== tab.id ? () => handleClick(tab.id) : undefined}
-                        style={{ cursor: selectedId === tab.id ? 'default' : 'pointer' }}
-                    >
-                        <div 
-                            className={styles.check}
-                            style={{
-                                backgroundColor: index < tabs.findIndex(t => t.id === selectedId) ? '#fcb654' : ''
-                            }}
-                        >
-                            <Icons icon={'check'}/>
-                        </div>
-                        {tab.title}
-                    </button>
-                ))}
-            </div>
-            <div className={styles.hr}>
-                <div className={styles.progress} style={{ width: `${lineWidth}%`}}></div>
-            </div>
-            <div className={styles.wrapperContent}>
-                {tabs.map(tab => (
-                    selectedId === tab.id && (
-                        <tab.component 
-                            key={tab.id} 
-                            setSelectedId={setSelectedId} 
-                            tabs={tabs} 
-                            formData={formData} 
-                            setFormData={setFormData}
-                            PaymentMethod={PaymentMethod}
-                            paymentMethod={paymentMethod}
-                            setPaymentMethod={setPaymentMethod}
-                            rules={rules}
-                            setRules={setRules}
-                        />
-                    )
-                ))}
-            </div>
+    if (tabId === tabs[0].id) {
+      setSelectedId(tabId);
+      return;
+    }
+
+    if (selectedId === tabs[0].id) {
+      setSelectedId(tabs[1].id);
+      scrollContainer.scrollLeft += 400;
+      return;
+    }
+
+    if (selectedId === tabs[1].id) {
+      if (
+        formData.firstName === '' ||
+        formData.lastName === '' ||
+        formData.address === '' ||
+        formData.city === '' ||
+        formData.postalCode === '' ||
+        formData.optionDeliveryMethod === ''
+      ) {
+        toast('Please fill in all fields');
+        return;
+      }
+
+      if (formData.phoneNumber === '') {
+        if (!/^\d{9}$/.test(formData['phoneNumber'])) {
+          toast('Please enter a valid phone number with 9 digits');
+          return;
+        }
+      }
+
+      if (formData.email === '') {
+        toast('Please fill in email field');
+        return;
+      }
+
+      if (!validator.isEmail(formData.email)) {
+        toast('Please enter a valid email address');
+        return;
+      }
+    }
+    setSelectedId(tabId);
+  };
+
+  const lineWidth =
+    (tabs.findIndex(t => t.id === selectedId) + 1) * (100 / tabs.length);
+
+  return (
+    <div>
+      <div className={styles.scrollContainer}>
+        <div className={styles.wrapper}>
+          {tabs.map((tab, index) => (
+            <button
+              className={styles.title}
+              key={tab.id}
+              onClick={
+                selectedId !== tab.id ? () => handleClick(tab.id) : undefined
+              }
+              style={{ cursor: selectedId === tab.id ? 'default' : 'pointer' }}
+            >
+              <div
+                className={styles.check}
+                style={{
+                  backgroundColor:
+                    index < tabs.findIndex(t => t.id === selectedId)
+                      ? '#fcb654'
+                      : '',
+                }}
+              >
+                <Icons icon={'check'} />
+              </div>
+              {tab.title}
+            </button>
+          ))}
         </div>
-    );
-}
+        <div className={styles.hr}>
+          <div
+            className={styles.progress}
+            style={{ width: `${lineWidth}%` }}
+          ></div>
+        </div>
+      </div>
+      <div className={styles.wrapperContent}>
+        {tabs.map(
+          tab =>
+            selectedId === tab.id && (
+              <tab.component
+                key={tab.id}
+                setSelectedId={setSelectedId}
+                tabs={tabs}
+                formData={formData}
+                setFormData={setFormData}
+                PaymentMethod={PaymentMethod}
+                paymentMethod={paymentMethod}
+                setPaymentMethod={setPaymentMethod}
+                rules={rules}
+                setRules={setRules}
+              />
+            ),
+        )}
+      </div>
+    </div>
+  );
+};

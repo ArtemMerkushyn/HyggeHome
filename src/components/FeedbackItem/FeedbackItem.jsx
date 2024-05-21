363d05faf9a6c513e2038315f619d2071ed7b8fe
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-05-22 01:56:03 +0200

    Added adaptive styles for review page

--- src/components/FeedbackItem/FeedbackItem.jsx
+++ src/components/FeedbackItem/FeedbackItem.jsx
@@ -4,31 +4,31 @@ import { Rating } from '../Rating/Rating';
 import Button from '../UI/Button/Button';
 
 const FeedbackItem = ({ data, index, onDeleteFeedback }) => {
-    const handleDelete = () => {
-        onDeleteFeedback(index);
-    };
+  const handleDelete = () => {
+    onDeleteFeedback(index);
+  };
 
-    return (
-        <div className={css.feedbackContainer}>
-            <div className={css.feedbackWrapper}>
-                <div className={css.name_and_date_container}>
-                    <h3 className={css.userName}>{data.user_name}</h3>
-                    <p className={css.date}>{data.date}</p>
-                </div>
-                <div className={css.rating_and_feedback_container}>
-                    <div className={css.rating_wrapper}>
-                        <Rating rating={data.rating} />
-                    </div>
-                    <p className={css.feedback_text}>{data.feedback}</p>
-                </div>
-            </div>
-            <div className={css.line}></div>
-            <div className={css.btns__wrapper}>
-                <Button text={"Delete feedback"} funcClick={handleDelete} />
-                <Button text={"Go to the product page"} />
-            </div>
+  return (
+    <div className={css.feedbackContainer}>
+      <div className={css.feedbackWrapper}>
+        <div className={css.name_and_date_container}>
+          <h3 className={css.userName}>{data.user_name}</h3>
+          <p className={css.date}>{data.date}</p>
         </div>
-    );
+        <div className={css.rating_and_feedback_container}>
+          <div className={css.rating_wrapper}>
+            <Rating rating={data.rating} />
+          </div>
+          <p className={css.feedback_text}>{data.feedback}</p>
+        </div>
+      </div>
+      <div className={css.line}></div>
+      <div className={css.btns__wrapper}>
+        <Button text={'Delete review'} funcClick={handleDelete} />
+        <Button text={'Go to the product page'} />
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackItem;
--- src/components/MyAccontNav/MyAccountNav.jsx
+++ src/components/MyAccontNav/MyAccountNav.jsx
@@ -5,47 +5,89 @@ import { useDispatch } from 'react-redux';
 import { setLoggedOut } from '../../redux/slices/userSlice';
 
 const MyAccountNav = () => {
-    const authorized = localStorage.getItem('token');
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    
-    const location = useLocation();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const authorized = localStorage.getItem('token');
+  const storedUser = JSON.parse(localStorage.getItem('user'));
 
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!authorized) {
-            navigate('/');
-        }
-    }, [authorized, navigate, ]);
+  useEffect(() => {
+    if (!authorized) {
+      navigate('/');
+    }
+  }, [authorized, navigate]);
 
-    return (
-        <aside className={css.navBar}>
-            {storedUser ? <h2 className={css.headedText}>{storedUser.name}</h2> : <h2 className={css.headedText}>User</h2>}
-            <div className={css.linkContainer}>
-                <NavLink to='/my-account/my-orders' className={css.navLink} style={{ color: location.pathname === '/my-account/my-orders' ? '#FCB654' : ''}}>
-                    My orders
-                </NavLink>
-                <NavLink to='/cart' className={css.navLink} style={{ color: location.pathname === '/my-account/my-cart' ? '#FCB654' : ''}}>
-                    My cart
-                </NavLink>
-                <NavLink to='/my-account/my-wishlist' className={css.navLink} style={{ color: location.pathname === '/my-account/my-wishlist' ? '#FCB654' : ''}}>
-                    My wishlist
-                </NavLink>
-                <NavLink to='/my-account/my-feedback' className={css.navLink} style={{ color: location.pathname === '/my-account/my-feedback' ? '#FCB654' : ''}}>
-                    My feedback
-                </NavLink>
-                <NavLink to='/my-account/my-delivery-information' className={css.navLink} style={{ color: location.pathname === '/my-account/my-delivery-information' ? '#FCB654' : ''}}>
-                    My delivery information
-                </NavLink>
-                <NavLink to='/'>
-                    <button className={css.logOut_Button} onClick={() => dispatch(setLoggedOut())}>
-                        Log Out
-                    </button>
-                </NavLink>
-            </div>
-        </aside>
-    );
+  return (
+    <aside className={css.navBar}>
+      {storedUser ? (
+        <h2 className={css.headedText}>{storedUser.name}</h2>
+      ) : (
+        <h2 className={css.headedText}>User</h2>
+      )}
+      <div className={css.linkContainer}>
+        <NavLink
+          to="/my-account/my-orders"
+          className={css.navLink}
+          style={{
+            color:
+              location.pathname === '/my-account/my-orders' ? '#FCB654' : '',
+          }}
+        >
+          My orders
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className={css.navLink}
+          style={{
+            color: location.pathname === '/my-account/my-cart' ? '#FCB654' : '',
+          }}
+        >
+          My cart
+        </NavLink>
+        <NavLink
+          to="/my-account/my-wishlist"
+          className={css.navLink}
+          style={{
+            color:
+              location.pathname === '/my-account/my-wishlist' ? '#FCB654' : '',
+          }}
+        >
+          My wishlist
+        </NavLink>
+        <NavLink
+          to="/my-account/my-feedback"
+          className={css.navLink}
+          style={{
+            color:
+              location.pathname === '/my-account/my-feedback' ? '#FCB654' : '',
+          }}
+        >
+          My reviews
+        </NavLink>
+        <NavLink
+          to="/my-account/my-delivery-information"
+          className={css.navLink}
+          style={{
+            color:
+              location.pathname === '/my-account/my-delivery-information'
+                ? '#FCB654'
+                : '',
+          }}
+        >
+          My delivery information
+        </NavLink>
+        <NavLink to="/">
+          <button
+            className={css.logOut_Button}
+            onClick={() => dispatch(setLoggedOut())}
+          >
+            Log Out
+          </button>
+        </NavLink>
+      </div>
+    </aside>
+  );
 };
 
 export default MyAccountNav;

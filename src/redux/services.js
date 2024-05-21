39d532954a0c27e73b52245b9c0ad22773de9cad
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-05-21 21:47:24 +0300

    changed the base url on the servieces file

--- src/redux/services.js
+++ src/redux/services.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const servicesApi = createApi({
   reducerPath: 'servicesApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://tc299.vercel.app',
+    baseUrl: 'https://tc299.onrender.com',
   }),
   endpoints: builder => ({
     getCandles: builder.query({

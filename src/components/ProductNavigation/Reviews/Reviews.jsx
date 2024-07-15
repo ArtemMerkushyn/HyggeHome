de5d7ac91a40786370c9af3fcc2168f21e7b3be5
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-07-15 21:13:13 +0300

    wrote an adaptation for different screens for reviews component

--- src/components/ProductNavigation/Reviews/Reviews.jsx
+++ src/components/ProductNavigation/Reviews/Reviews.jsx
@@ -5,26 +5,20 @@ import items from './db.json';
 
 export default function Reviews() {
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
-      <ul>
+    <div>
         {items.map((item, index) => (
-          <div key={index}>
-            <li className={styles.review} style={{ display: 'flex' }}>
-              <div className={styles.review__left} style={{ width: '332px' }}>
+            <div key={index} className={styles.review}>
+              <div className={styles.review__left}>
                 <p className={styles.name}>{item.name}</p>
                 <p className={styles.date}>{item.date}</p>
               </div>
-              <div className={styles.review__right} style={{ width: '743px' }}>
+              <div className={styles.review__right}>
                 <Rating rating={item.rating} />
                 <p className={styles.comment}>{item.comment}</p>
               </div>
-            </li>
             <div className={styles.rectangle}></div>
           </div>
         ))}
-      </ul>
       <button className={styles.button}>Open filters</button>
     </div>
   );
